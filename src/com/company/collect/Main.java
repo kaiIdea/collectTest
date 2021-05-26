febf19e7e3f2d6844293e9b0c1d935936a2ac9da
GEEX1428 <yangkaikai@geexfinance.com>
2021-05-26 11:46:00 +0800

    init project

---
+++ src/com/company/collect/Main.java
@@ -0,0 +1,12 @@
+package com.company.collect;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        List<String> list = new ArrayList<>();
+    }
+}
