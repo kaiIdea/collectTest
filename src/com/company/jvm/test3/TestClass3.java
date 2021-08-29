0e0612ac6d124fdc84f4d45ddd253923d0658b59
YangKai <loveyouyk_mm@163.com>
2021-08-29 15:22:48 +0800

    虚拟机字节码指令表上传

---
+++ src/com/company/jvm/test3/TestClass3.java
@@ -0,0 +1,15 @@
+package com.company.jvm.test3;
+
+/**
+ * @Auther: YangKai
+ * @Date: 2021/8/29 14:51
+ * @Description:
+ */
+public class TestClass3 {
+
+    private String text = "Hello";
+    private String text1 = "World";
+    private String text2 = text+text1;
+
+
+}
